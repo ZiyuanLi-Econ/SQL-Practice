a7b94221f676c4910187bffc8cfdee54e8e39fad
ZiyuanLi-Econ <1796494629@qq.com>
2026-08-21 19:20:23 +0200

    Clean up Day16 and update README
    * Remove draft query from Day16
    
    * Update practice log for Day16

--- Day16 – 21.08.2026.sql
+++ Day16 – 21.08.2026.sql
@@ -6,17 +6,9 @@ on p.employee_id=e.employee_id
 group by p.project_id
 
 --1633
-select r.contest_id, round(sum(case when r.contest_id is null then 0 else 1 end)*100/(select count(distinct uu.user_id) from users as uu),2) as percentage
-from users as u
-left join register as r
-on u.user_id=r.user_id
-group by r.contest_id
-order by percentage desc, r.contest_id asc
-
 select r.contest_id, round(count(*)*100/(select count(distinct uu.user_id) from users as uu),2) as percentage
 from users as u
 join register as r
 on u.user_id=r.user_id
 group by r.contest_id
 order by percentage desc, r.contest_id asc
-
